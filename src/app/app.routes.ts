5b034a97cc76e168cded3c1d368211dae19e3be0
Piotrek Kaczalko <116281448+pkaczalko@users.noreply.github.com>
2023-12-12 23:59:40 +0100

    D

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,38 +1,12 @@
 import {Routes} from "@angular/router";
-import {DetailsComponent} from "./details/details.component";
-import {ListComponent} from "./list/list.component";
-import {AddPersonComponent} from "./add-person/add-person.component";
-import {NotFoundComponent} from "./not-found/not-found.component";
+import {TasksComponent} from "./tasks/tasks.component";
+
 
 
 const routeConfig: Routes = [
-    {
-        path: '',
-        component: ListComponent,
-        title: 'list',
-        pathMatch: 'full'
-    },
-  {
-    path: 'list',
-    component: ListComponent,
-    title: 'list',
-  },
-  {
-    path: 'details/:id',
-    component: DetailsComponent,
-    title: 'details',
-  },
-    {
-        path: 'add',
-        component: AddPersonComponent,
-        title: 'Add person',
-    },
-    {
-        path: '**',
-        component: NotFoundComponent,
-        title: 'Not found',
-    }
+    {path: 'tasks', component: TasksComponent},
+    {path: '', redirectTo: '/tasks', pathMatch: 'full'},
+];
 
-]
 
 export default routeConfig;
